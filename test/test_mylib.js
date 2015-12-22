c25fce0c204eceecf5fb0dd8e134503d079259d3
Romain TOUZE <romain.touze@gmail.com>
2015-12-22 11:07:30 +0100

    refactoring

--- test/test_mylib.js
+++ test/test_mylib.js
@@ -1,6 +1,11 @@
 describe('Bowling game', function () {
+    var game = null;
+
+    beforeEach(function () {
+        game = new bowling.BowlingGame();
+    });
+
     it('should return a score of 0 if no pin hit', function () {
-        var game = new bowling.BowlingGame();
         for (var i = 0; i < 20; i++ ) {
             game.roll(0);
         }
@@ -8,7 +13,6 @@ describe('Bowling game', function () {
     });
 
     it('should return 60 when always hit 3 pins', function () {
-        var game = new bowling.BowlingGame();
         for (var i = 0; i < 20; i++ ) {
             game.roll(3);
         }
